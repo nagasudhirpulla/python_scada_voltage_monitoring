cb34436d28365b2b7de91a1e76f7a29b62ad09e2
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-09-16 15:13:16 +0530

    'changes

--- scada_fetcher.py
+++ scada_fetcher.py
@@ -1,3 +1,11 @@
 import random
+import requests
 def fetchScadaPntRealData(pntId):
+    urlStr = "http://localhost:62448/api/values/real"
+    paramsObj = {'pnt':pntId} 
+    r = requests.get(url = urlStr, params = paramsObj)
+    data = r.json()
+    return data['dval']
+
+def fetchScadaPntRandData(pntId):
     return random.randint(-50, 50)
