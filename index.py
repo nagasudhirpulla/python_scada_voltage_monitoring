2a49f1e2ee524ae4efbf614d621c8e37a35341f4
NagaSudhir Pulla <nagasudhirpulla@gmail.com>
2021-04-20 15:03:31 +0530

    Update index.py

--- index.py
+++ index.py
@@ -30,10 +30,19 @@ stateRepLineSeparator = "-------------------------------------------"
 for st in ['gj', 'mh', 'mp', 'cg', 'cs']:
     stateHvBusMessage = hvDetector.generateMessage(st, isForHigh=True)
     stateOffBrsMessage = brsDetector.generateMessage(st, isOn=False)
-    stateIctsMessage = ictsDetector.generateMessage(st, isFlowReverse=False)
+    stateIctsMessage = ictsDetector.generateMessage(st, isFlowReverse=True)
     stateGtsMessage = gtsDetector.generateMessage(st, isFlowReverse=False)
-    messageStr += '{0} Report\n\n{1}\n\n{2}\n\n{3}\n\n{4}\n{5}\n\n'.format(stateNames[st], stateHvBusMessage,
-                                                                           stateOffBrsMessage, stateIctsMessage, stateGtsMessage, stateRepLineSeparator)
-
+    messageStr += '{0} Substations Summary\n\n{1}\n\n{2}\n\n{3}\n\n{4}\n{5}\n\n'.format(stateNames[st], stateHvBusMessage,
+                                                    stateOffBrsMessage, stateIctsMessage, stateGtsMessage, stateRepLineSeparator)
+messageStr += """May please refer clause 5.2(S), 6.4.12, 6.6.3 in this regard.
+Request to
+1. Take up with concerned S/S to reduce MVAR flow from LV side to HV side by switching off capacitors,
+2. To switch on all B/Rs and L/Rs
+3. To absorb maximum Vars at all generating stations as per capability curve so as to maintain at least voltage reduced to 405kV on EHV side of generator bus.
+4. To explore possibility of running more machines at Koyna Stage IV in condenser mode, presently two units are running in condenser mode.
+5. To run mchines at Koyna stage-I & II in condenser mode
+6. To take up with wind generators connected on LV side to operate in Voltage control mode not to generate VARs
+7. And to implement Any other necessary measure to control voltages to safe values
+"""
 with open(outFilePath, 'w') as f:
     f.write(messageStr)
